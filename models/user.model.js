8820ee0aec321f7ffe11b38ab8779f2fe6d04118
Donny <donny.hyon@madetech.com>
2022-08-04 14:39:48 +0100

    html connected to css

--- models/user.model.js
+++ models/user.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const userSchema = mongoose.Schema({
 	name: String,
-	content: String,
+	content: String
 })
 
 module.exports = mongoose.model("userSchema", userSchema)
--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.port || 3000
 const mongoose = require('mongoose')
-const routes = require('./routes/users')
-
+const routes = require('../routes/users')
+const path = require('path')
 
 require('dotenv').config()
 
@@ -18,6 +18,7 @@ app.listen(port, () => {
     console.log(`Sever listening on port: ${port}`)
 })
 
+app.use(express.static(path.join(__dirname, '../public')))
 app.use('/api', routes)
     
 db.on('error', (error) => {
@@ -26,4 +27,8 @@ db.on('error', (error) => {
 
 db.once('connected',() => {
     console.log('Database Connected')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'pages/index.html'))
 })
